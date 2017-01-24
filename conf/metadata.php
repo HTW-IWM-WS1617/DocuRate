ee88f834c5c777f5a0fdd6d7eb5f46f659d34063
TillBu <tillbussmann@googlemail.com>
2017-01-24 17:12:59 +0100

    fixed folder order

---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the dokurate2 plugin
+ *
+ * @author Gruppe HTWdokurate2 <tillbussmann@googlemail.com>
+ */
+
+
+//$meta['fixme'] = array('string');
+
---
+++ lang/en/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * english language file for dokurate2 plugin
+ *
+ * @author Gruppe HTWdokurate2 <tillbussmann@googlemail.com>
+ */
+
+// keys need to match the config setting name
+// $lang['fixme'] = 'FIXME';
+
+
+
+//Setup VIM: ex: et ts=4 :
